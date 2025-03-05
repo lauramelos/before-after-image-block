f5050fe1d5e927bab27533ae028aa0551e10d2dd
Laura Melo <lauramelos@gmail.com>
2025-03-05 16:59:12 +0000

    Initial Create Block structure

---
+++ build/beforeafterimages/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '48c015b07a7c37c68f49');
---
+++ build/beforeafterimages/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a6fbd235bff646bfb221');
